3b0959bab39ff587db1f763bcc560b4a62b94eb7
ZarifAhsan <mdzarifahsan@gmail.com>
2020-08-30 05:26:08 +0600

    Added navigation

--- app/src/main/java/com/example/miwok/MainActivity.java
+++ app/src/main/java/com/example/miwok/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void openNumbersList(View view) {
+        Intent intent = new Intent(this, NumbersActivity.class);
+        startActivity(intent);
+    }
+
+    public void openFamilyList(View view) {
+        Intent intent = new Intent(this, FamilyActivity.class);
+        startActivity(intent);
+    }
+
+    public void openPhrasesList(View view) {
+        Intent intent = new Intent(this, PhrasesActivity.class);
+        startActivity(intent);
+    }
+
+    public void openColorsList(View view) {
+        Intent intent = new Intent(this, ColorsActivity.class);
+        startActivity(intent);
+    }
+
 }
