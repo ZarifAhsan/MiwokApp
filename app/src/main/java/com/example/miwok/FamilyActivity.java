05eb0108890661dbe8cf1a69bef5d25964e7d575
ZarifAhsan <mdzarifahsan@gmail.com>
2020-08-31 17:12:44 +0600

    All activity updated

--- app/src/main/java/com/example/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/miwok/FamilyActivity.java
@@ -3,12 +3,39 @@ package com.example.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.word_list);
+
+        ArrayList<Word> familyWords = new ArrayList<>();
+        createFamilyList(familyWords);
+        createListView(familyWords);
+    }
+
+    private void createFamilyList(ArrayList<Word> arrayList) {
+        arrayList.add(new Word("father", "әpә", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("mother", "әṭa", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("son", "angsi", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("daughter", "tune", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("older brother", "taachi", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("younger brother", "chalitti", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("older sister", "teṭe", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("younger sister", "kolliti", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("grandmother", "ama", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("grandfather", "paapa", R.drawable.ic_launcher_background));
+    }
+
+    private void createListView(ArrayList<Word> arrayList) {
+        WordAdapter wordAdapter = new WordAdapter(this, arrayList);
+
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(wordAdapter);
     }
 }
--- app/src/main/java/com/example/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/miwok/PhrasesActivity.java
@@ -3,12 +3,41 @@ package com.example.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phrases);
+        setContentView(R.layout.word_list);
+
+        ArrayList<Word> phrasesWords = new ArrayList<>();
+
+        createPhrasesList(phrasesWords);
+        createListView(phrasesWords);
     }
+
+    private void createPhrasesList(ArrayList<Word> arrayList) {
+        arrayList.add(new Word("Where are you going?", "minto wuksus?", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("What is your name?", "tinnә oyaase'nә?", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("My name is...", "oyaaset...", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("How are you feeling?", "michәksәs?", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("I'm feeling good.", "kuchi achit", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("Are you coming?", "әәnәs'aa?", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("Yes, I'm coming.", "hәә’ әәnәm", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("I'm coming", "әәnәm", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("Let's go.", "yoowutis", R.drawable.ic_launcher_background));
+        arrayList.add(new Word("Come here.", "әnni'nem", R.drawable.ic_launcher_background));
+    }
+
+    private void createListView(ArrayList<Word> arrayList) {
+        WordAdapter wordAdapter = new WordAdapter(this, arrayList);
+
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(wordAdapter);
+    }
+
 }
